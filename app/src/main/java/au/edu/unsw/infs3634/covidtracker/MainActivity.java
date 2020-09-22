e5d1ab12055176aeba2bd1fea75b09590c5396a6
Lachlangolsby <49334505+Lachlangolsby@users.noreply.github.com>
2020-09-22 15:06:57 +1000

    Week 2 Tutorial Commit

--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
@@ -2,7 +2,10 @@ package au.edu.unsw.infs3634.covidtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //1. Define a button in the layout file and assign it a unique ID
+        //From the onCreate method of an activity, find the button
+        Button button = findViewById(R.id.btLaunchActivty);
+
+        //2. Create activity (detailActivity) and set up layout
+
+
+        //5. Create an onClickListener to launch detail activity. Run and test app.
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.launchDetailActivity("test");
+
+            }
+        });
+
+        }
+        //3. Create launchDetailActivity method. Uses explicit intent to start activity
+    private void launchDetailActivity (String message) {
+        Intent intent = new Intent ( this, DetailActivity.class);
+        intent.putExtra(DetailActivity.INTENT_MESSAGE, message);
+        startActivity(intent);
+
+
     }
 }
