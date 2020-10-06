03ebaeaf5b1bffeee2df95a285397073e10ac1b4
Lachlangolsby <49334505+Lachlangolsby@users.noreply.github.com>
2020-10-06 15:26:31 +1100

    Week 4 commit.

--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
@@ -1,6 +1,7 @@
 package au.edu.unsw.infs3634.covidtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,27 +9,30 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private CountryAdapter mAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //1. Define a button in the layout file and assign it a unique ID
-        //From the onCreate method of an activity, find the button
-        Button button = findViewById(R.id.btLaunchActivty);
 
+        mRecyclerView = findViewById(R.id.rvList);
+        mRecyclerView.setHasFixedSize(true);
 
-
-        //2. Create an onClickListener to launch detail activity. Run and test app.
-        button.setOnClickListener(new View.OnClickListener() {
+        CountryAdapter.RecyclerViewClickListener listener = new CountryAdapter.RecyclerViewClickListener() {
             @Override
-            public void onClick(View v) {
-                MainActivity.this.launchDetailActivity("US");
-
+            public void onClick(View view, String countryCode) {
+                launchDetailActivity(countryCode);
             }
-        });
+        };
 
-        }
+        mAdapter = new CountryAdapter(Country.getCountries(),listener);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
         //3. Create launchDetailActivity method. Uses explicit intent to start activity
     private void launchDetailActivity (String message) {
         Intent intent = new Intent ( this, DetailActivity.class);
